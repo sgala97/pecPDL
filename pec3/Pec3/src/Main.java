58d45d7f6a62b7de3b595bf9c260da12728fc059
hugo <hugo.garciac@edu.uah.es>
2021-12-30 03:15:09 +0100

    grafo funciona 100%

--- pec3/Pec3/src/Main.java
+++ pec3/Pec3/src/Main.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws Exception
     {
+
         InputStream is = new FileInputStream("H:\\OneDrive\\OneDrive - Universidad de Alcala\\3º TERCERO\\Procesardores del Lenguaje\\Practica3\\pecPDL\\pec3\\Pec3\\src\\Programa.pseint");
         CharStream input = CharStreams.fromStream(is);
         pseint_lexer lexer = new pseint_lexer(input);
@@ -20,11 +21,16 @@ public class Main {
         ParseTreeWalker walker = new ParseTreeWalker();
         walker.walk(listener,tree);
         imprimirHijos(tablaDeSimbolos.getPrograma().getListaHijos(),0);
-        //System.out.println("Nombre Funciones : "+tablaDeSimbolos.getPrograma().getHijos(0).getTipo());
+        Grafo grafo = new Grafo();
+        grafo.generarGrafo(tablaDeSimbolos.getPrograma());
+           //System.out.println("Nombre Funciones : "+tablaDeSimbolos.getPrograma().getHijos(0).getTipo());
     }
 
 
+    public static String a(int a){
 
+        return String.valueOf(a);
+    }
     public static void imprimirHijos(ArrayList<Bloque> bloques, int n) {
         for (Bloque bloque: bloques)
         {
